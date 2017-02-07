88e0a0e3933f6d8662f61497104cd92ff2b1bf64
serenariback <serenariback@CHS-A104-DSK-03.CHS.chatham-nj.org>
2017-02-07 13:33:03 -0500

    it looks terrible

--- HangmanView.java
+++ HangmanView.java
@@ -1,6 +1,9 @@
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 import acm.graphics.GCanvas;
 
@@ -14,11 +17,18 @@ public class HangmanView extends JFrame {
 
 	public final int CANVAS_WIDTH = 600;
 	public final int CANVAS_HEIGHT = 400;
+	
 	public GCanvas canvas = new GCanvas();
+	private JLabel title = new JLabel("HANGMAN");
+	private JLabel subtitle = new JLabel("The Fish Edition");
 	
 	public HangmanView(HangmanController controller) {
 		add(canvas, BorderLayout.CENTER);
 		setSize(CANVAS_WIDTH, CANVAS_HEIGHT);
+		
+		//the title
+		canvas.add(title, 300,200);
+		canvas.add(subtitle,300,220);
 		
     }
 }
