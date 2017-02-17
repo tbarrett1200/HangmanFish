32f207e7d2da72d1627133164b09efa72fab77b4
Thomas Barrett <tbarrett1200@icloud.com>
2017-02-17 10:39:56 -0500

    erf

--- view/BackgroundPanel.java
+++ view/BackgroundPanel.java
@@ -1,38 +1,3 @@
-<<<<<<< HEAD
-/*
- * File: BackgroundPanel.java
- * Author: Thomas Barrett
- * Created: Feb 16, 2017
- */
-package view;
-
-import java.awt.Graphics;
-import java.awt.Image;
-import java.io.File;
-import java.io.IOException;
-
-import javax.imageio.ImageIO;
-import javax.swing.JPanel;
-
-public class BackgroundPanel extends JPanel {
-
-    Image background;
-    
-    public BackgroundPanel(String img) {
-	try {
-	    background = ImageIO.read(new File(img));
-	} catch (IOException e) {
-	    // TODO Auto-generated catch block
-	    e.printStackTrace();
-	}
-    }
-    
-    @Override
-    public void paintComponent(Graphics g) {
-	g.drawImage(background, 0, 0, getWidth(), getHeight(), null);
-    }
-}
-=======
 package view;
 
 import java.awt.Graphics;
@@ -70,4 +35,3 @@ public class BackgroundPanel extends JPanel {
 		g.drawImage(image, 0, 0, getWidth(), getHeight(), null);
 	}
 }
->>>>>>> branch 'master' of https://github.com/tbarrett1200/HangmanFish.git
